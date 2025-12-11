dc05b9dbaa7696dbc6445dc7605f164184e7808a
SSG-Control <ssg-control@SSG-Controls-iMac.local>
2025-12-11 16:15:48 +0000

    Added report in the directory, included all members work together

--- Analysis.R
+++ Analysis.R
@@ -63,6 +63,7 @@ print(t_res)
 # 6. PLOTS
 # ----------------------------------------------------
 
+# Following are the appropriate plots:
 
 # Full time-series plot
 p1 <- ggplot(df, aes(x = Date, y = Close, color = period)) +
